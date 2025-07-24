ec754fe2b83cf90d891377727d5574c930ca6d86
Austin Okonkwo <austinokonkwo1995@gmail.com>
2025-07-24 18:10:57 +0100

    Added update journal

--- journals/urls.py
+++ journals/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import JournalList, JournalCreate, JournalDetail
+from .views import JournalList, JournalCreate, JournalDetail, JournalUpdate, DeleteView
 
 urlpatterns = [
     path("", JournalList.as_view(), name="home"),
     path("create/", JournalCreate.as_view(), name="journal-create"),
     path("<int:pk>/", JournalDetail.as_view(), name="journal-detail"),
+    path("<int:pk>/edit/", JournalUpdate.as_view(), name="journal-update"),
+    #path("<int:pk>/delete", DeleteView.as_view(), name="journal-delete"),
 ]
--- journals/views.py
+++ journals/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from . models import Journal
 
@@ -18,4 +19,14 @@ class JournalCreate(CreateView):
 class JournalDetail(DetailView):
     model = Journal
     template_name = "journals/journal_detail.html"
+
+class JournalUpdate(UpdateView):
+    model = Journal
+    template_name = "journals/journal_update.html"
+    fields = "__all__"
+    success_url = "http://127.0.0.1:8000/"
     
+""" class JournalDelete(DeleteView):
+    model = Journal
+    template_name = "journals/journal_delete.html"
+    success_url = reverse_lazy('journal_list') """
